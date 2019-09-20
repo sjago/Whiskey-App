4a084f911c23a107af884550473a22268faea8de
Sean <sjago88@gmail.com>
2019-09-19 21:32:25 -0400

    19/9

--- app/controllers/whiskeys_controller.rb
+++ app/controllers/whiskeys_controller.rb
@@ -11,15 +11,30 @@ class WhiskeysController < ActionController::Base
   def create
 
     attrs = params.require(:whiskey).permit(:name, :description)
-    @whiskey = Whiskey.new(attrs)
-    @whiskey.save
-    redirect_to whiskey_path(@whiskey)
+    @whiskeys = Whiskey.new(attrs)
+    @whiskeys.save
+    redirect_to whiskey_path(@whiskeys)
+    
+  end
+
+  def edit
+
+    @whiskeys = Whiskey.find(params[:id])
     
   end
 
   def show
     
-    @whiskey = Whiskey.find(params[:id])
+    @whiskeys = Whiskey.find(params[:id])
+    
+  end
+
+  def update
+
+    @whiskeys = Whiskey.find(params[:id])
+    attrs = params.require(:whiskey).permit(:name, :description)
+    @whiskeys.update(attrs)
+    redirect_to whiskey_path(@whiskeys)
     
   end
 
